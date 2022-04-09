fdbd790c9fc298cd795a3501e0c8c06a5dbebf06
Fahad <fahad@guidedways.com>
2022-04-09 04:13:25 +0400

    xcframework support

---
+++ Sources/TPInAppReceipt.h
@@ -0,0 +1,18 @@
+//
+//  TPInAppReceipt.h
+//  TPInAppReceipt
+//
+//  Created by Fahad on 09/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TPInAppReceipt.
+FOUNDATION_EXPORT double TPInAppReceiptVersionNumber;
+
+//! Project version string for TPInAppReceipt.
+FOUNDATION_EXPORT const unsigned char TPInAppReceiptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPInAppReceipt/PublicHeader.h>
+
+
